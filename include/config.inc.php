87ee54e3bbb5c1bfeca1aa2f3729e99fa0d9222e
Christopher Hammond <christopherhammond13@gmail.com>
2015-12-12 21:47:20 +0000

    Fixed a secret issue

---
+++ include/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "projectnadia");
+define("DB_PASSWORD", "RSNEaX5ru9xE4vr3");
+define("DB_DATABASE", "projectnadia");
+
+define("YOUTUBE_KEY", "AIzaSyC9Q-4i1bKIwdJn4RX3LJ9nsXK7TYCb1AE");
+
+?>

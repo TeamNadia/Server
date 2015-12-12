bee5e7ef823265b3a4b31265c02e70b13a6728d7
Christopher Hammond <christopherhammond13@gmail.com>
2015-12-12 22:32:19 +0000

    Some actually useful code

--- submitsong.php
+++ submitsong.php
@@ -1,34 +1,85 @@
 <?php
-include_once("include/config.inc.php");
+require_once("include/config.inc.php");
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/google-api-php-client/src');
+require_once('Google/Client.php');
+require_once('Google/Service/YouTube.php');
 
 if (isset($_GET['screen']) && isset($_GET['pin']) && isset($_GET['artist']))
 {
-	// Connect up to YouTube!
-	
+	// Before anything, check that the screen pin is correct
 	$mysqli	= new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 	$screen	= $mysqli->real_escape_string($_GET['screen']);
 	$pin	= $mysqli->real_escape_string($_GET['pin']);
-	$url	= $mysqli->real_escape_string($_GET['url']);
 	
-	$query	= "SELECT * FROM queue WHERE url = '$url'";
-	$result	= $mysqli->query($query);
-	if ($result->num_rows > 0)
+	$screenquery = "SELECT * FROM screens WHERE id = $screen";
+	$screenresult = $mysqli->query($screenquery);
+	if ($screenresult->num_rows > 0)
 	{
-		$row = $result->fetch_array(MYSQLI_ASSOC);
-		$upvotes = (int) $row['upvotes'];
-		$upvotes++;
-		
-		$id = $row['id'];
-		
-		$query2 = "UPDATE queue SET upvotes = $upvotes WHERE id = $id";
-		$mysqli->query($query2);
-		
-		echo "SUCCESS";
+		$screenrow = $screenresult->fetch_array(MYSLI_ASSOC);
+		if ($screenrow['pin'] == $pin)
+		{
+			// Screen pin is ok, so go ahead and actually do some shit
+			$artist	= $mysqli->real_escape_string($_GET['artist']);
+			$track = "";
+			if (isset($_GET['track']))
+				$track = $mysqli->real_escape_string($_GET['track']);
+			
+			$searchquery = $artist;
+			if ($track != "")
+				$searchgquery .= " - " . $track;
+				
+			// Connect up to YouTube	
+			$googleclient = new Google_Client();
+			$googleclient->setDeveloperKey(YOUTUBE_KEY);
+			$youtube = new Google_Service_YouTube($googleclient);
+			
+			$searchresponse = $youtube->search->listSearch('id,snippet', array(
+				'q' => $searchquery,
+				'maxResults' => 5
+			));
+			
+			$videos = array();
+			foreach($searchresponse['items'] as $searchresult)
+			{
+				if ($searchresult['id']['kind'] == 'youtube#video')
+				{
+					$videos[] = "https://youtube.com/watch?v=" . $searchresult['id']['videoId'];
+				}
+			}
+			
+			$url = $videos[0];
+			
+			$query	= "SELECT * FROM queue WHERE url = '$url'";
+			$result	= $mysqli->query($query);
+			if ($result->num_rows > 0)
+			{
+				$row = $result->fetch_array(MYSQLI_ASSOC);
+				$upvotes = (int) $row['upvotes'];
+				$upvotes++;
+				
+				$id = $row['id'];
+				
+				$query2 = "UPDATE queue SET upvotes = $upvotes WHERE id = $id";
+				$mysqli->query($query2);				
+			}
+			else
+			{
+				$query2 = "INSERT INTO queue VALUES(NULL, $screen, $url, 1, NULL";
+				$mysqli->query($query2);
+			}
+			echo "SUCCESS";
+		}
+		else
+		{
+			echo "FAIL";
+		}
 	}
 	else
 	{
-		
-		
+		echo "FAIL";
 	}
+}
+else
+{
+	echo "FAIL";
 }

b46cfbf5fc7e1d41354e9a98b1b23f02093f185d
Christopher Hammond <christopherhammond13@gmail.com>
2015-12-13 00:52:42 +0000

    Added screen listing (JSON) code

---
+++ getscreens.php
@@ -0,0 +1,22 @@
+<?php
+require("include/config.inc.php");
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+$screens = array();
+
+$query	= "SELECT * FROM screens";
+$result	= $mysqli->query($query);
+
+while ($row = $result->fetch_array(MYSQLI_ASSOC))
+{
+	$screens[] = array
+	(
+		"id"		=> $row['id'],
+		"name"		=> $row['name'],
+		"location"	=> $row['location']
+	);
+}
+
+$output = json_encode($screens);
+
+echo $output;

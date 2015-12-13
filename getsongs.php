8cee4c06407b1cec5cefac6679b7f90d6e29e99e
Christopher Hammond <christopherhammond13@gmail.com>
2015-12-13 01:21:34 +0000

    Add getsongs.php sanity check when a fake screen ID is given

--- getsongs.php
+++ getsongs.php
@@ -11,20 +11,27 @@ if (isset($_GET['screen']))
 	$query	= "SELECT * FROM queue WHERE screen = '$screen' ORDER BY votes DESC, timestamp ASC";
 	$result	= $mysqli->query($query);
 	
-	while ($row = $result->fetch_array(MYSQLI_ASSOC))
+	if ($result->num_rows > 0)
 	{
-		$tracks[] = array
-		(
-			"id"		=> $row['id'],
-			"artist"	=> $row['artist'],
-			"track"		=> $row['track'],
-			"thumbnail"	=> $row['thumbnail'],
-			"votes"		=> $row['votes']
-		);
+		while ($row = $result->fetch_array(MYSQLI_ASSOC))
+		{
+			$tracks[] = array
+			(
+				"id"		=> $row['id'],
+				"artist"	=> $row['artist'],
+				"track"		=> $row['track'],
+				"thumbnail"	=> $row['thumbnail'],
+				"votes"		=> $row['votes']
+			);
+		}
+		
+		$output = json_encode($tracks);
+		echo $output;
 	}
-	
-	$output = json_encode($tracks);
-	echo $output;
+	else
+	{
+		echo "SCREENFAIL";
+	}
 }
 else
 {

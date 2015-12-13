a7f815537a9a5587037aa859e3cd2ea14417cada
Christopher Hammond <christopherhammond13@gmail.com>
2015-12-13 01:40:54 +0000

    Added URL grabber

---
+++ geturl.php
@@ -0,0 +1,29 @@
+<?php
+require_once("include/config.inc.php");
+
+if (isset($_GET['screen']) && isset($_GET['id']))
+{
+	$mysqli	= new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	$screen	= $mysqli->real_escape_string($_GET['screen']);
+	$songid	= $mysqli->real_escape_string($_GET['id']);
+	
+	$query	= "SELECT * FROM queue WHERE id = '$id' AND screen = '$screen'";
+	$result	= $mysqli->query($query);
+	
+	if ($result->num_rows > 0)
+	{
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		$url = $row['url'];
+		echo $url;
+		$query2 = "DELETE FROM queue WHERE id = '$id'";
+		$mysqli->query($query2);
+	}
+	else
+	{
+		echo "EVERYTHINGFAIL";
+	}
+}
+else
+{
+	echo "NOTHINGFAIL";
+}

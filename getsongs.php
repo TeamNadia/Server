f993d25d6797b8bcaa6b6738b90c83efa9e4474e
Christopher Hammond <christopherhammond13@gmail.com>
2015-12-13 01:20:16 +0000

    Add listing of songs for a given screen

---
+++ getsongs.php
@@ -0,0 +1,32 @@
+<?php
+require_once("include/config.inc.php");
+
+if (isset($_GET['screen']))
+{
+	$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	$screen = $mysqli->real_escape_string($_GET['screen']);
+	
+	$tracks = array();
+	
+	$query	= "SELECT * FROM queue WHERE screen = '$screen' ORDER BY votes DESC, timestamp ASC";
+	$result	= $mysqli->query($query);
+	
+	while ($row = $result->fetch_array(MYSQLI_ASSOC))
+	{
+		$tracks[] = array
+		(
+			"id"		=> $row['id'],
+			"artist"	=> $row['artist'],
+			"track"		=> $row['track'],
+			"thumbnail"	=> $row['thumbnail'],
+			"votes"		=> $row['votes']
+		);
+	}
+	
+	$output = json_encode($tracks);
+	echo $output;
+}
+else
+{
+	echo "SCREENFAIL";
+}
